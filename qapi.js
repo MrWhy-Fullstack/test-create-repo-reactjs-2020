8644ff3cc756bcf53bb854d6c91aafa0c6642911
Phạm Công Toán <73781967+PhamCongToan-ReactJS@users.noreply.github.com>
2020-11-16 19:40:16 +0700

    Changes

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fb27297badc03001fb1436e",
+				"5fb27297badc03001fb14370"
+			]
+		},
+		"5fb27297badc03001fb1436e": {
+			"id": "5fb27297badc03001fb1436e",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fb27297badc03001fb14370": {
+			"id": "5fb27297badc03001fb14370",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
